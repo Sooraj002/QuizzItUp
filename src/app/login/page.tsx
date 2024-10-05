c0f6a7d78ed3b6b5179ac9a58e6b42373c441cdf
Sooraj002 <soorajnambiar8@gmail.com>
2024-10-05 16:35:34 +0530

    fixed

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,18 +4,7 @@ import { useRouter } from "next/navigation";
 
 export default function Login() {
   const router = useRouter();
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const email = event.currentTarget.elements.namedItem(
-      "email"
-    ) as HTMLInputElement;
-    const password = event.currentTarget.elements.namedItem(
-      "password"
-    ) as HTMLInputElement;
-    const token = { email: email.value, password: password.value };
-    localStorage.setItem("token", JSON.stringify(token));
-    router.push("/");
-  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <button
